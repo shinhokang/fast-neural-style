15533926aad65bd24d2730385d8c54d5f609890b
shinhokang <wis.shinho.kang@gmail.com>
2017-02-04 14:16:15 +0100

    test

--- first_test.sh
+++ first_test.sh
@@ -1,7 +1,6 @@
-/usr/bin/time -v th fast_neural_style.lua \
+time th fast_neural_style.lua \
   -model models/instance_norm/la_muse.t7 \
   -input_dir images/content \
   -output_dir out \
-  -gpu 0 \
   -cudnn_benchmark 1 \
   -timing 1 \
