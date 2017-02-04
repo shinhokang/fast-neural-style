db55a42372ac40bb5ff87a7170ed1579bc9fcb64
shinhokang <wis.shinho.kang@gmail.com>
2017-02-04 15:44:07 +0100

    task3 test script

---
+++ tasks/task3/task3_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+declare -a style_arr=("composition-vii-1913" "femme_nue_assise" "udnie")
+declare -a image_arr=("peak_obama_from_wash-mon" "peak-trump-from-wash-mon" "Pulp-Fiction-005" "Pulp-Fiction-467" "The-Grand-Budapest-Hotel-058")
+
+for style in "${style_arr[@]}"
+do
+  for image in "${image_arr[@]}"
+  do
+    /usr/bin/time -v th fast_neural_style.lua \
+        -model tasks/task3/models/$style.t7 \
+        -input_image images/content/$image.jpg \
+        -output_image tasks/task3/output/${style}_${image}.png \
+        -gpu 3 \
+        -cudnn_benchmark 1 \
+        -timing 1
+  done
+done
