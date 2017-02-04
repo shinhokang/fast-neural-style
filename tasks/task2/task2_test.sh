77e30d69632d674c67e1bfec95523ee81d44e83f
shinhokang <wis.shinho.kang@gmail.com>
2017-02-04 19:10:34 +0100

    task2 script

--- tasks/task2/task2_test.sh
+++ tasks/task2/task2_test.sh
@@ -1,15 +1,18 @@
-declare -a arr=( "la_muse" "starry_night" "composition-vii-1913" "femme_nue_assise" "udnie" )
+#!/bin/bash
 
-for style in "${arr[@]}"
+declare -a style_arr=("composition-vii-1913" "femme_nue_assise" "udnie")
+declare -a image_arr=("peak_obama_from_wash-mon" "peak-trump-from-wash-mon" "Pulp-Fiction-005" "Pulp-Fiction-467" "The-Grand-Budapest-Hotel-058")
+
+for style in "${style_arr[@]}"
 do
-	echo ""
-	echo "Style training: $style"  
-  /usr/bin/time -v th fast_neural_style.lua \
-    -model tasks/models_1and2/ .t7 \
-    -input_image images/content/chicago.jpg \
-    -output_image training/12_09_11_01_2017/testout.png \
-    -gpu 1 \
-    -cudnn_benchmark 1 \
-    -timing 1 \
-
+  for image in "${image_arr[@]}"
+  do
+    /usr/bin/time -v th fast_neural_style.lua \
+        -model tasks/task2/models/$style.t7 \
+        -input_image images/content/$image.jpg \
+        -output_image tasks/task2/output/${style}_${image}.png \
+        -gpu 3 \
+        -cudnn_benchmark 1 \
+        -timing 1
+  done
 done
